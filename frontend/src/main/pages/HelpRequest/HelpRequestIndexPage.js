55b740905ff12df002c35c94c392bc9eb7f26c55
Victoria Reed <victoria@Victorias-MacBook-Air-2.local>
2022-05-15 18:32:03 -0700

    vr- replaced placeholder w table

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -1,13 +1,28 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestTable from 'main/components/HelpRequest/HelpRequestTable';
+import { useCurrentUser } from 'main/utils/currentUser';
 
 export default function HelpRequestIndexPage() {
+  
+  const currentUser = useCurrentUser()
+
+  const { data: helpRequest, error: _error, status: _status} = 
+    useBackend(
+       // Stryker disable next-line all : don't test internal caching of React Query
+       ["/api/ucsbdiningcommons/all"],
+       // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+       { method: "GET", url: "/api/helprequest/all" },
+       []
+
+    );
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Help Request Item</h1>
-        <p>
-          Hello this is an index page
-        </p>
+          <HelpRequestTable helpRequest={helpRequest} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
