bbc2543d8ef960dd0e0abfbd571369e0c4c65a84
aripo99 <a_polakof@umail.ucsb.edu>
2022-05-17 13:19:59 -0700

    ap - add article fixtures and add article table to stories

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,35 @@
+const articlesFixtures = {
+    oneArticle: {
+        "dateAdded": "2022-04-17",
+        "email": "a@ucsb.edu",
+        "explanation": "none",
+        "title": "None",
+        "url": "https://none",
+    },
+    threeArticles: [
+        {
+            "dateAdded": "2022-04-17",
+            "email": "a1@ucsb.edu",
+            "explanation": "none1",
+            "title": "None1",
+            "url": "https://none1",
+        },
+        {
+            "dateAdded": "2022-04-17",
+            "email": "a2@ucsb.edu",
+            "explanation": "none2",
+            "title": "None2",
+            "url": "https://none2",
+        },
+        {
+            "dateAdded": "2022-04-17",
+            "email": "a3@ucsb.edu",
+            "explanation": "none3",
+            "title": "None3",
+            "url": "https://none3",
+        } 
+    ]
+};
+
+
+export { articlesFixtures };
