d056f52bd97b56d9acc23d3fe8e0aec17f0a9573
aripo99 <a_polakof@umail.ucsb.edu>
2022-05-17 13:20:56 -0700

    ap - add article table placehoder

---
+++ frontend/src/main/components/Article/ArticlesTable.js
@@ -0,0 +1,43 @@
+import OurTable, { _ButtonColumn} from "main/components/OurTable";
+import { _useBackendMutation } from "main/utils/useBackend";
+import { hasRole } from "main/utils/currentUser";
+
+export default function ArticlesTable({ articles, currentUser }) {
+
+    const columns = [
+        {
+            Header: 'Date Added',
+            accessor: 'dateAdded', 
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'URL',
+            accessor: 'url',
+        }
+    ];
+
+    const testid = "ArticlesTable";
+
+    const columnsIfAdmin = [
+        ...columns
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={articles}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
