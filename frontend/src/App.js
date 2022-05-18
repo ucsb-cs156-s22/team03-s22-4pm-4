97ef95e9c34736a2415ce42e8e519d2a57257d1f
Wang Haoran(Robin) <notrealrobin@gmail.com>
2022-05-17 22:10:35 -0700

    merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/profile' element={<ProfilePage />} />
         {hasRole(currentUser, "ROLE_ADMIN") && (
@@ -53,6 +52,15 @@ function App() {
               exact
               path='/diningCommons/list'
               element={<DiningCommonsIndexPage />}
+            />
+          </>
+        )}
+        {hasRole(currentUser, "ROLE_USER") && (
+          <>
+            <Route
+              exact
+              path='/ucsbdiningcommonsmenuitem/list'
+              element={<UCSBDiningCommonsMenuItemIndexPage />}
             />
           </>
         )}
@@ -93,75 +101,6 @@ function App() {
             <Route exact path='/articles/list' element={<ArticleIndexPage />} />
           </>
         )}
-        {hasRole(currentUser, "ROLE_USER") && (
-          <>
-            <Route
-              exact
-              path='/ucsborganization/list'
-              element={<OrganizationIndexPage />}
-            />
-          </>
-        )}
-=======
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/profile" element={<ProfilePage />} />
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && <Route exact path="/admin/users" element={<AdminUsersPage />} />
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/todos/list" element={<TodosIndexPage />} />
-              <Route exact path="/todos/create" element={<TodosCreatePage />} />
-              <Route exact path="/todos/edit/:todoId" element={<TodosEditPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/diningCommons/list" element={<DiningCommonsIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/ucsbdiningcommonsmenuitem/list" element={<UCSBDiningCommonsMenuItemIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/helprequest/list" element={<HelpRequestIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/ucsbdates/list" element={<UCSBDatesIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && (
-            <>
-              <Route exact path="/ucsbdates/edit/:id" element={<UCSBDatesEditPage />} />
-              <Route exact path="/ucsbdates/create" element={<UCSBDatesCreatePage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/articles/list" element={<ArticleIndexPage />} />
-            </>
-          )
-        }
-
->>>>>>> main
       </Routes>
     </BrowserRouter>
   );
